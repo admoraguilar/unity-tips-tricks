4c77759e7c59454c08627084db881a97e6e35014
Admor Aloysious Aguilar <admor.aguilar@gmail.com>
2021-01-12 13:24:52 +0800

    * Create 00001-screenshot tip

---
+++ Assets/00001-screenshot/Scripts/CaptureScreenshot.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace UnityTipsTricks
+{
+	public class CaptureScreenshot : MonoBehaviour
+	{
+		public int superSize = 2;
+		private int _shotIndex = 0;
+
+		private void Update()
+		{
+			if(Input.GetKeyDown(KeyCode.A)) {
+				ScreenCapture.CaptureScreenshot($"Screenshot{_shotIndex}.png", superSize);
+				_shotIndex++;
+			}
+		}
+	}
+}
