5bc0ca793e71bac1aeb521e6e2868eefa91cef81
Admor Aloysious Aguilar <admor.aguilar@gmail.com>
2021-01-23 20:50:53 +0800

    * Create 00002 - timescale tip

---
+++ Assets/00002-timescale/Scripts/ModifyTimeScale.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+namespace UnityTipsTricks
+{
+	public class ModifyTimeScale : MonoBehaviour
+	{
+		// https://answers.unity.com/questions/36509/if-timescale-is-lowered-how-can-i-aviod-to-get-the.html
+		private void SetTimeScale(float scale)
+		{
+			Time.timeScale = scale;
+			Time.fixedDeltaTime = scale * .02f;
+		}
+
+		private void Update()
+		{
+			// 1f = Normal time
+
+			if(Input.GetKeyDown(KeyCode.Q)) {
+				// Slow-down time
+				SetTimeScale(.5f);
+			}
+
+			if(Input.GetKeyDown(KeyCode.W)) {
+				// Freeze-time
+				SetTimeScale(0f);
+			}
+
+			if(Input.GetKeyDown(KeyCode.E)) {
+				// Faster time
+				SetTimeScale(2f);
+			}
+		}
+	}
+}
