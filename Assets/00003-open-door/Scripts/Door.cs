50f8b410ad7d8b65efb24cc135e366eb12319e0d
Admor Aloysious Aguilar <admor.aguilar@gmail.com>
2021-02-13 21:06:28 +0800

    * Initial 00003-open-door folder * Folder reorganization

---
+++ Assets/00003-open-door/Scripts/Door.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UObject = UnityEngine.Object;
+
+namespace UnityTipsTricks.P00003
+{
+	public class Door : MonoBehaviour
+	{
+		[SerializeField]
+		private UObject _obj = null;
+	}
+}
